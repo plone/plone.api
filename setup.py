b1d088c333d06877f9810f72a01de651fbfc7bf7
Andreas Zeidler <witsch@plone.org>
2012-02-25 01:55:10 +0100

    make way for plone.api, from #plonekonf

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '1.0a1'
+
+setup(name='plone.api',
+      version=version,
+      description='A Plone API.',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      license='GPL version 2',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['plone'],
+      include_package_data=True,
+      platforms='Any',
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+)
