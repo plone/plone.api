824902edc6d38b5a4d18055e061048005be8d7cc
Andreas Zeidler <witsch@plone.org>
2012-02-25 02:24:20 +0100

    add `get_request`

--- src/plone/api/__init__.py
+++ src/plone/api/__init__.py
@@ -1,6 +1,12 @@
 from zope.app.component.hooks import getSite
+from zope.globalrequest import getRequest
 
 
 def get_site():
     """ Return the Plone Site object. """
     return getSite()
+
+
+def get_request():
+    """ Return the current request. """
+    return getRequest()
--- src/plone/api/tests.py
+++ src/plone/api/tests.py
@@ -11,3 +11,10 @@ class APITests(TestCase):
         site = get_site()
         self.assertEqual(site.getPortalTypeName(), 'Plone Site')
         self.assertEqual(site.getId(), 'plone')
+
+    def test_get_request(self):
+        from plone.api import get_request
+        request = get_request()
+        from ZPublisher.HTTPRequest import HTTPRequest
+        self.assertTrue(isinstance(request, HTTPRequest))
+        self.assertEqual(request.getURL(), 'http://nohost')
