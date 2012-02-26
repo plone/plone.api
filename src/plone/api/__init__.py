26d88611bc90d4446ba4b380eaa9869cd27362df
Nejc Zupan <nejc.zupan@gmail.com>
2012-02-26 19:23:53 +0100

    narrative and API docs for utilities

--- src/plone/api/__init__.py
+++ src/plone/api/__init__.py
@@ -1,11 +1,6 @@
-from Products.CMFCore.interfaces import ISiteRoot
 from Products.CMFPlone.utils import getToolByName
 from zope.app.component.hooks import getSite
-from zope.component import getUtility
 from zope.globalrequest import getRequest
-
-import random
-import string
 
 
 def get_site():
@@ -41,3 +36,41 @@ def get_tool(name=None, *args):
 
     site = get_site()
     return getToolByName(site, name)
+
+
+def send_email(sender=None, recipient=None, subject=None, body=None, *args):
+    """Send an email.
+
+    :param sender: [required] Email sender, 'from' field.
+    :type sender: string
+    :param recipient: [required] Email recipient, 'to' field.
+    :type recipient: string
+    :param subject: [required] Subject of the email.
+    :type subject: string
+    :param body: [required] Body text of the email
+    :type body: string
+    """
+
+    if args:
+        raise ValueError('Positional arguments are not allowed!')
+
+    if not body and not recipient and not sender and not subject:
+        raise ValueError
+
+    site = get_site()
+    encoding = site.getProperty('email_charset', 'utf-8')
+
+    # The mail headers are not properly encoded we need to extract
+    # them and let MailHost manage the encoding.
+    if isinstance(body, unicode):
+        body = body.encode(encoding)
+
+    host = getToolByName(get_site(), 'MailHost')
+    host.send(
+        body,
+        recipient,
+        sender,
+        subject=subject,
+        charset=encoding,
+        immediate=True
+    )
