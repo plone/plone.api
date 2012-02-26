a394012ab8e3e91280d67da9cf1ad063f2a466a0
Nejc Zupan <nejc.zupan@gmail.com>
2012-02-26 19:05:59 +0100

    add docstrings to utility methods

--- src/plone/api/__init__.py
+++ src/plone/api/__init__.py
@@ -9,76 +9,35 @@ import string
 
 
 def get_site():
-    """Return the Plone Site object."""
+    """Get the Plone Site object out of thin air without importing fancy
+    Interfaces and doing multi adapter lookups.
+
+    :returns: Plone Site object
+    """
     return getSite()
 
 
 def get_request():
-    """Return the current request."""
+    """Get the current request object out of thin air without importing fancy
+    Interfaces and doing multi adapter lookups.
+
+    :returns: Request object
+    """
     return getRequest()
 
 
-def create(container, type, id, **kwargs):
-    """Create content of the given type."""
-    fti = get_site().portal_types[type]
-    return fti.constructInstance(container, id, **kwargs)
+def get_tool(name=None, *args):
+    """Get a portal tool in a simple way.
 
+    :param email: [required] Name of the tool you want.
+    :type email: string
+    :returns: portal tool
+    """
+    if args:
+        raise ValueError('Positional arguments are not allowed!')
 
-class Tools(object):
-    """Portal tools."""
+    if not name:
+        raise ValueError
 
-    def __getitem__(self, name):
-        site = get_site()
-        return getToolByName(site, name)
-
-tools = Tools()
-
-
-class Users(object):
-    """User management."""
-
-    def __getitem__(self, name):
-        return tools['portal_membership'].getMemberById(name)
-
-users = Users()
-
-
-def create_user(username, email, password=None, **kwargs):
-    """Create user."""
     site = get_site()
-    registration = getToolByName(site, 'portal_registration')
-
-    # if password is not set, generate one
-    if not password:
-        random.seed()
-        sample = random.sample(string.letters + string.digits, 8)
-        password = ''.join(sample)
-
-    # build user properties and add username into it
-    properties = kwargs
-    properties['username'] = username
-    properties['email'] = email
-
-    return registration.addMember(username, password, properties=properties)
-
-
-def change_password(userid, password):
-    """Change the user's password."""
-    site = get_site()
-    memberdata = getToolByName(site, 'portal_memberdata')
-    member = memberdata.getMemberById(userid)
-    member._setPassword(password)
-
-
-def send_email(body="", recipient="", sender="", subject=""):
-    """Send an email."""
-    encoding = getUtility(ISiteRoot).getProperty('email_charset', 'utf-8')
-
-    # The mail headers are not properly encoded we need to extract
-    # them and let MailHost manage the encoding.
-    if isinstance(body, unicode):
-        body = body.encode(encoding)
-
-    host = getToolByName(get_site(), 'MailHost')
-    host.send(body, recipient, sender, subject=subject, charset=encoding,
-              immediate=True)
+    return getToolByName(site, name)
