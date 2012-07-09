5d1156dc45f3abb4b2317cc09c9ef9e717738c16
Kim Chee Leong <kc@leong.nl>
2012-07-09 20:04:16 +0200

    added unit test for creating content

--- src/plone/api/content.py
+++ src/plone/api/content.py
@@ -47,11 +47,12 @@ def create(container=None,
         raise ValueError('You have to provide either the ``id`` or the '
                          '``title`` attribute')
 
-    if not strict and id in container.keys():
-        id = None
+    if strict and not id:
+        raise ValueError('You have to provide the ``id`` attribute when using strict')
+
 
     # Create a temporary id if the id is not given
-    content_id = id or str(random.randint(0, 99999999))
+    content_id = strict and id or str(random.randint(0, 99999999))
 
     if title:
         kwargs['title'] = title
@@ -65,10 +66,12 @@ def create(container=None,
         # rename-after-creation and such
         content.processForm()
 
-    if not id:
+    # Set the correct id, based on given id or title
+    if not strict:
         # Create a new id from title
         chooser = INameChooser(container)
-        new_id = chooser.chooseName(title, content)
+        derived_id = id or title
+        new_id = chooser.chooseName(derived_id, content)
         # kacee: we must do a commit, else the renaming fails because the object isn't in the zodb.
         # Thus if it is not in zodb, there's nothing to move. We should choose a correct id when
         # the object is created.
--- src/plone/api/tests/test_content.py
+++ src/plone/api/tests/test_content.py
@@ -2,6 +2,7 @@
 """Tests for plone.api content."""
 import mock
 import unittest
+from zExceptions import BadRequest
 
 from plone.api import content
 
@@ -9,22 +10,42 @@ class TestPloneApiContent(unittest.TestCase):
     """Unit test on IWWBSearcher using mocked service results."""
 
     def test_create_contraints(self):
-        """ Test content creation """
+        """ Test the contrainst when creating content """
 
         # This will definitely fail
         self.assertRaises(ValueError, content.create)
 
-        # Now we pass type and id but leave out the container parameter
+        # Check the contraints for the type container
         self.assertRaises(ValueError, content.create, type='Document', id='test-doc')
 
-        # Now we pass container and id but leave out the type parameter
+        # Check the contraints for the type parameter
         container = mock.Mock()
         self.assertRaises(ValueError, content.create, container=container, id='test-doc')
 
-        # Now check the contraints for id and title
+        # Check the contraints for id and title parameters
         self.assertRaises(ValueError, content.create, container=container, type='Document')
 
+        # Check the contraints for the strict parameter, it required the id parameter
+        self.assertRaises(
+            ValueError, content.create,
+            container=container, type='Document', strict=True, title='Spam')
 
+
+    def test_create_strict(self):
+        """" Test the content creating with the strict parameter. When using strict the given id
+        is enforced when adding content.
+        """
+
+        container = mock.MagicMock()
+#        container.invokeFactory.return_value = 'foo'
+
+        # Create a page with strict option
+        page = content.create(container=container, type='Page', id='strict-page', strict=True)
+
+        # Check the calls done on the mock container
+        container.invokeFactory.assert_called_with('Page', 'strict-page')
+        container.__getitem__.assert_called_with('strict-page')
+        self.assertEqual(len(container.mock_calls), 3)
 
 
     def test_get(self):
