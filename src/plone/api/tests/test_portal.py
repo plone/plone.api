5f0b1545b26e8abcff7372a16b9dea9b8daa29b6
Maurits van Rees <maurits@vanrees.org>
2012-07-11 17:21:41 +0200

    test_portal: tests need to begin with 'test_'

--- src/plone/api/tests/test_portal.py
+++ src/plone/api/tests/test_portal.py
@@ -22,15 +22,15 @@ class TestPloneApiPortal(unittest.TestCase):
     def test_url(self):
         self.assertEqual(portal.url(), 'http://nohost/plone')
 
-    def get_tool_constraints(self):
+    def test_get_tool_constraints(self):
         """ Test the constraints for getting a tool. """
 
         # When no parameters are given an error is raised
         self.assertRaises(ValueError, portal.get_tool)
 
-    def get_tool(self):
+    def test_get_tool(self):
         self.assertEqual(portal.get_tool('portal_catalog'),
                          getToolByName(self.portal, 'portal_catalog'))
         self.assertEqual(portal.get_tool('portal_membership'),
                          getToolByName(self.portal, 'portal_membership'))
-        self.assertEqual(portal.get_tool('non_existing'), None)
+        self.assertRaises(AttributeError, portal.get_tool, 'non_existing')
