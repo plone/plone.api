c82c6d50ea464c90b03b708527f668da64fb9209
Kim Chee Leong <kc@leong.nl>
2012-07-11 16:36:13 +0200

    Added tests for adding/deleting users to groups

--- src/plone/api/tests/test_group.py
+++ src/plone/api/tests/test_group.py
@@ -30,24 +30,33 @@ class TestPloneApiGroup(unittest.TestCase):
         assert self.group_tool.getGroupById('spam')
 
         # Group with title and description
-        api.group.create(
+        bacon_group = api.group.create(
             groupname='bacon',
             title='Bacon',
             description='Hmm bacon good!'
         )
-        bacon_group = self.group_tool.getGroupById('bacon')
 
-        self.assertEqual(bacon_group.getGroupTitleOrName(), 'Bacon')
-        self.assertEqual(bacon_group.getProperty('description'), 'Hmm bacon good!')
+        self.assertEqual(
+            bacon_group,
+            self.group_tool.getGroupById('bacon')
+        )
+        self.assertEqual(
+            bacon_group.getGroupTitleOrName(),
+            'Bacon'
+        )
+        self.assertEqual(
+            bacon_group.getProperty('description'),
+            'Hmm bacon good!'
+        )
 
         # Group with roles and groups
-        api.group.create(
+        ham_group = api.group.create(
             groupname='ham',
             roles=['Editor', ],
             groups=['Reviewer', ]
         )
-        ham_group = self.group_tool.getGroupById('ham')
-
+        group = self.group_tool.getGroupById('ham')
+        self.assertEqual(ham_group, group)
         assert 'Editor' in ham_group.getRoles()
         assert 'Reviewer' in ham_group.getMemberIds()
 
@@ -58,7 +67,10 @@ class TestPloneApiGroup(unittest.TestCase):
         api.group.create(groupname='bacon')
         bacon = api.group.get(groupname='bacon')
 
-        self.assertEqual(bacon, self.group_tool.getGroupById('bacon'))
+        self.assertEqual(
+            bacon,
+            self.group_tool.getGroupById('bacon')
+        )
 
     def test_get_all(self):
         """ Test adding of a group """
@@ -89,3 +101,92 @@ class TestPloneApiGroup(unittest.TestCase):
 
         api.group.delete('bacon')
         assert not api.group.get('bacon')
+
+    def test_add_user_contraints(self):
+        """ Test the constraints when a user is added to a group """
+
+        group, user = mock.Mock(), mock.Mock()
+
+        # Arguments ``groupname`` and ``group`` are also mutually exclusive.
+        self.assertRaises(
+            ValueError,
+            api.group.add_user,
+            groupname='staff', group=group
+        )
+        # Arguments ``username`` and ``user`` are mutually exclusive.
+        self.assertRaises(
+            ValueError,
+            api.group.add_user,
+            username='staff', user=user
+        )
+        self.assertRaises(ValueError, api.group.add_user, groupname='staff')
+        self.assertRaises(ValueError, api.group.add_user, username='jane')
+
+    def test_add_user(self):
+        """ Test adding a user to a group """
+
+        api.group.create(groupname='staff')
+        api.user.create(email='jane@plone.org', username='jane')
+        api.user.create(email='bob@plone.org', username='bob')
+
+        # Add user by username to group
+        api.group.add_user(groupname='staff', username='bob')
+
+        user = api.user.get(username='jane')
+        group = api.group.get(groupname='staff')
+
+        # Add user by user object to group
+        api.group.add_user(group=group, user=user)
+
+        assert 'staff' in api.user.get_groups(username='bob')
+        assert 'staff' in api.user.get_groups(username='jane')
+
+        assert 'bob' in group.getMemberIds()
+        assert 'jane' in group.getMemberIds()
+
+
+    def test_delete_user_contraints(self):
+        """ Test the constraints when a user is deleted from a group """
+
+        group, user = mock.Mock(), mock.Mock()
+
+        # Arguments ``groupname`` and ``group`` are also mutually exclusive.
+        self.assertRaises(
+            ValueError,
+            api.group.delete_user,
+            groupname='staff', group=group
+        )
+        # Arguments ``username`` and ``user`` are mutually exclusive.
+        self.assertRaises(
+            ValueError,
+            api.group.delete_user,
+            username='staff', user=user
+        )
+        self.assertRaises(ValueError, api.group.delete_user, groupname='staff')
+        self.assertRaises(ValueError, api.group.delete_user, username='jane')
+
+
+    def test_delete_user(self):
+        """ Test deleting a user from a group """
+
+        api.group.create(groupname='staff')
+        api.user.create(email='jane@plone.org', username='jane')
+        api.user.create(email='bob@plone.org', username='bob')
+        api.group.add_user(groupname='staff', username='jane')
+        api.group.add_user(groupname='staff', username='bob')
+
+        # Delete user by username from group
+        api.group.delete_user(groupname='staff', username='bob')
+
+        group = api.group.get(groupname='staff')
+        user = api.user.get(username='jane')
+
+        # Delete user by user object from group
+        api.group.delete_user(group=group, user=user)
+
+        assert 'staff' not in api.user.get_groups(username='bob')
+        assert 'staff' not in api.user.get_groups(username='jane')
+
+        assert 'bob' not in group.getMemberIds()
+        assert 'jane' not in group.getMemberIds()
+
