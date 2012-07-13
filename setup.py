18495957d6c1fe858b321b2c29cd147056b8eab7
Nejc Zupan <nejc.zupan@gmail.com>
2012-07-13 03:36:25 +0200

    release 0.1a1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0a1'
+version = '0.1a1'
 
 setup(name='plone.api',
       version=version,
