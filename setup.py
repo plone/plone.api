9176e4cbdb510183ee811221d125364796ddab2a
Nejc Zupan <nejc.zupan@gmail.com>
2012-09-03 20:45:39 +0200

    maybe order matters?

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ setup(
     install_requires=[
         'setuptools',
         'Plone',  # this is needed so we can pull docstrings into Sphinx
-        'Zope2',  # this also
-        'plone.app.uuid',       # this also
         'zope.globalrequest',   # this also
+        'Zope2',                # this also
+        'plone.app.uuid',       # this also
         'zope.location<4.0.0',  # needed so autodoc can import
     ],
     extras_require={
