1f6219ff494f2dfd40f2298ebf3384b3d0f2a536
Nejc Zupan <nejc.zupan@gmail.com>
2012-09-03 21:18:19 +0200

    RTD dependencies

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     install_requires=[
         'setuptools',
         'Plone',  # this is needed so we can pull docstrings into Sphinx
+        'plone.i18n',             # this also
         'Products.Archetypes',    # this also
         'zope.app.container',     # this also
         'zope.globalrequest',     # this also
