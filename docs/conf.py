19548fc8e4bbc3760a0de65178a032ae21a154f4
Nejc Zupan <nejc.zupan@gmail.com>
2012-10-13 16:10:02 +0200

    RTD: blblblbl

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['lxml', 'zope.location']
+MOCK_MODULES = ['lxml', 'zope.site.hooks']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
