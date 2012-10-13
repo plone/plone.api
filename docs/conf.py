7e6f9eb0ac27f546e0becd6bab619fae5161bc1d
Nejc Zupan <nejc.zupan@gmail.com>
2012-10-13 15:51:13 +0200

    RTD: more mocked modules

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['lxml']
+MOCK_MODULES = ['lxml', 'zope.location']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
