92d0170dcc0810c8cb016df084131434a99afe7d
Jessica Henderson <JessN@Jessicas-MacBook-Pro.local>
2013-01-26 00:36:35 +0100

    pull out some common code into separate functions in preparation for new decorators

--- src/plone/api/validation.py
+++ src/plone/api/validation.py
@@ -5,6 +5,30 @@ import inspect
 from plone.api.exc import MissingParameterError
 
 
+def _get_arg_spec(func, validator_args):
+    """ Get the arguments specified in the function spec
+    and check that the decorator doesn't refer to non-existant args """
+    signature_args, _, _, _ = inspect.getargspec(func)
+
+    extra_args = set(validator_args) - set(signature_args)
+    if extra_args:
+        raise ValueError(
+            "Validator for %s refers to parameters that are \
+not part of its signature: %s" % (
+            func.__name__, ", ".join(extra_args),))
+
+    return signature_args
+
+
+def _get_supplied_args(signature_params, args, kwargs):
+    """ make arg_name:value pairs for all args that have been passed in """
+    supplied_args = {}
+    for i in range(len(args)):
+        supplied_args[signature_params[i]] = args[i]
+    supplied_args.update(kwargs)
+    return supplied_args
+
+
 def required_parameters(*required_params):
     """ A decorator that tests whether all of the specified parameters
     have been supplied and are not None
@@ -20,25 +44,15 @@ def required_parameters(*required_params):
     def _required_parameters(func):
         """ The actual decorator """
 
-        signature_params, _, _, _ = inspect.getargspec(func)
-        extra_params = set(required_params) - set(signature_params)
-        if extra_params:
-            raise ValueError(
-                "required_parameters: %s requires parameters \
-that are not part of its signature: %s"
-                % (func.__name__, ", ".join(extra_params)))
+        signature_params = _get_arg_spec(func, required_params)
 
         def wrapped(*args, **kwargs):
             """ The wrapped function """
 
-            # make arg_name:value pairs for all args that have been passed in
-            assigned_params = {}
-            for i in range(len(args)):
-                assigned_params[signature_params[i]] = args[i]
-            assigned_params.update(kwargs)
+            supplied_args = _get_supplied_args(signature_params, args, kwargs)
 
             for p in required_params:
-                if p not in assigned_params or assigned_params[p] is None:
+                if p not in supplied_args or supplied_args[p] is None:
                     raise MissingParameterError(
                         "Missing required parameter: %s" % p)
 
