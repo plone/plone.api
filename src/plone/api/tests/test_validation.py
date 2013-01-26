1ad4b15f23e1578ed34afa16ac7aed22693b214c
Jessica Henderson <JessN@Jessicas-MacBook-Pro.local>
2013-01-26 12:36:32 +0100

    test for _get_supplied_args

--- src/plone/api/tests/test_validation.py
+++ src/plone/api/tests/test_validation.py
@@ -4,6 +4,7 @@
 from plone.api.exc import InvalidParameterError
 from plone.api.exc import MissingParameterError
 from plone.api.tests.base import INTEGRATION_TESTING
+from plone.api.validation import _get_supplied_args as _gsa
 from plone.api.validation import mutually_exclusive_parameters
 from plone.api.validation import required_parameters
 
@@ -33,6 +34,34 @@ class TestPloneAPIValidation(unittest.TestCase):
             mutually_exclusive_parameters('arg1', 'wibble', 'wobble'),
             undecorated_func)
 
+    def test_get_supplied_args(self):
+        """Test that positional and keyword args are recognised correctly"""
+        # the arguments specified in the function signature
+        signature = ('arg1', 'arg2', 'arg3')
+
+        # test that positional args are recognised correctly
+        result = _gsa(signature, ('foo', 'wibble'), {})
+        self.assertEquals(set(result), set(('arg1', 'arg2')))
+
+        # test that keyword args are recognised correctly
+        result = _gsa(
+            signature, (), {'arg1': 'foo', 'arg2': 'wibble'})
+        self.assertEquals(set(result), set(('arg1', 'arg2')))
+
+        # test that a mixture of args are recognised correctly
+        result = _gsa(signature, ('foo',), {'arg2': 'wibble'})
+        self.assertEquals(set(result), set(('arg1', 'arg2')))
+
+        # test that None-valued positional args are ignored
+        result = _gsa(
+            signature, ('foo', None), {})
+        self.assertEquals(set(result), set(('arg1',)))
+
+        # test that None-valued keyword args are ignored
+        result = _gsa(
+            signature, (), {'arg1': None, 'arg2': 'wibble'})
+        self.assertEquals(set(result), set(('arg2',)))
+
     def test_single_keyword_arg_provided(self):
         """Test for passing a single required parameter
         as a keyword argument
@@ -53,13 +82,6 @@ class TestPloneAPIValidation(unittest.TestCase):
         """
         _func = required_parameters('arg1')(undecorated_func)
         self.assertRaises(MissingParameterError, _func)
-
-    def test_single_arg_is_none(self):
-        """Test that MissingParameterError is raised if the
-        single required parameter is None
-        """
-        _func = required_parameters('arg1')(undecorated_func)
-        self.assertRaises(MissingParameterError, _func, None)
 
     def test_one_missing_one_provided(self):
         """Test that MissingParameterError is raised if only one of the
