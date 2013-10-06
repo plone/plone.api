e936f3a3d8db7e1ab0e6e8bcbf9b7749369c137b
cewing <cris@crisewing.com>
2013-10-06 13:05:06 -0300

    support removing user from group by userid

--- src/plone/api/group.py
+++ src/plone/api/group.py
@@ -163,9 +163,11 @@ def add_user(
 
 @mutually_exclusive_parameters('groupname', 'group')
 @at_least_one_of('groupname', 'group')
-@mutually_exclusive_parameters('username', 'user')
-@at_least_one_of('username', 'user')
-def remove_user(groupname=None, group=None, username=None, user=None):
+@mutually_exclusive_parameters('username', 'user', 'userid')
+@at_least_one_of('username', 'user', 'userid')
+def remove_user(
+    groupname=None, group=None, username=None, user=None, userid=None
+):
     """Remove the user from a group.
 
     Arguments ``groupname`` and ``group`` are mutually exclusive. You can
@@ -182,14 +184,21 @@ def remove_user(groupname=None, group=None, username=None, user=None):
     :type username: string
     :param user: User to delete from the group.
     :type user: MemberData object
+    :param userid: Userid of the user to delete from the group
+    :type userid: string
     :raises:
         ValueError
     :Example: :ref:`group_remove_user_example`
     """
-    user_id = username or user.id
-    group_id = groupname or group.id
+    if username:
+        user = user_get(username=username)
+
+    if not userid:
+        userid = user.id
+
+    groupid = groupname or group.id
     portal_groups = portal.get_tool('portal_groups')
-    portal_groups.removePrincipalFromGroup(user_id, group_id)
+    portal_groups.removePrincipalFromGroup(userid, groupid)
 
 
 @mutually_exclusive_parameters('groupname', 'group')
