40df4f1e3165acb32dfd35efda5751a7972e635d
Roel Bruggink <roel@jaroel.nl>
2015-07-07 19:16:06 +0200

    We do not support non-setuptools enabled environments.

--- src/plone/__init__.py
+++ src/plone/__init__.py
@@ -1,7 +1,2 @@
 # -*- coding: utf-8 -*-
-# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:  # pragma: no cover
-    from pkgutil import extend_path  # pragma: no cover
-    __path__ = extend_path(__path__, __name__)  # pragma: no cover
+__import__('pkg_resources').declare_namespace(__name__)
