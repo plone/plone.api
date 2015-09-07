3fc68136f836257e5b2bde8a4aaa20dffa8cf5c9
Roel Bruggink <roel@jaroel.nl>
2015-09-07 09:40:49 +0200

    In preperation for inclusion with Plone core: there's no need to depend on Products.CMFPlone. It will be there.

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Products.CMFPlone',
         'Products.statusmessages',
         'decorator',
         'plone.app.uuid',
