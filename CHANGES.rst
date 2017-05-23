1bf6fb18ae115aadaf8bb3c65e557c4b195a5654
Maurits van Rees <maurits@vanrees.org>
2017-05-23 19:04:06 +0200

    Bumped version for feature release.
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,8 +1,8 @@
 Changelog
 =========
 
-1.6.2 (unreleased)
-------------------
+1.7 (unreleased)
+----------------
 
 Breaking changes:
 
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = \
     read('CHANGES.rst') + '\n\n' + \
     read('LICENSE')
 
-version = '1.6.2.dev0'
+version = '1.7.dev0'
 
 setup(
     name='plone.api',
