1fa03674842bacb71f6b656cfb94083157923cfa
Maurits van Rees <maurits@vanrees.org>
2017-05-23 19:04:20 +0200

    Preparing release 1.7
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.7 (unreleased)
+1.7 (2017-05-23)
 ----------------
 
 New features:
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = \
     read('CHANGES.rst') + '\n\n' + \
     read('LICENSE')
 
-version = '1.7.dev0'
+version = '1.7'
 
 setup(
     name='plone.api',
