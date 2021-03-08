82f183513ef5b20ca56737eaebdd21848c6e3739
Johannes Raggam <thetetet@gmail.com>
2021-03-08 08:54:18 +0100

    Condition on test not necessary anymore as Plone < 5.2 support was dropped.

--- src/plone/api/tests/test_content.py
+++ src/plone/api/tests/test_content.py
@@ -5,9 +5,7 @@ from Acquisition import aq_base
 from OFS.CopySupport import CopyError
 from OFS.event import ObjectWillBeMovedEvent
 from OFS.interfaces import IObjectWillBeMovedEvent
-from pkg_resources import parse_version
 from plone import api
-from plone.api import env
 from plone.api.content import _parse_object_provides_query
 from plone.api.content import NEW_LINKINTEGRITY
 from plone.api.tests.base import INTEGRATION_TESTING
@@ -43,9 +41,6 @@ except pkg_resources.DistributionNotFound:
     HAS_PACONTENTYPES = False
 else:
     HAS_PACONTENTYPES = True
-
-
-HAS_PLONE51 = parse_version(env.plone_version()) >= parse_version('5.1')
 
 
 class TestPloneApiContent(unittest.TestCase):
@@ -1055,10 +1050,6 @@ class TestPloneApiContent(unittest.TestCase):
         )
         self.assertEqual(len(brains), 1)
 
-    @unittest.skipUnless(
-        HAS_PLONE51,
-        'Negative queries only available since Products.ZCatalog 3.0a1 or Plone 5.1',
-    )
     def test_find_interface_dict__include_not_query(self):
         """Check if not query in object_provides is functional.
         """
