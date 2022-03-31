4208176b82fa3c7927d6f4d53751070e11ac30be
ksuess <k.suess@rohberg.ch>
2022-03-31 11:51:44 +0200

    documentation with sphinx-book-theme

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,16 @@ class Mock:
         else:
             return Mock()
 
+
 MOCK_MODULES = ['lxml']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_book_theme"
+
