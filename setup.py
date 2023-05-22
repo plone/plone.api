0beddf5d2d0cb3ee72edee4e5a4f9d3a6e47c5e0
Maurits van Rees <maurits@vanrees.org>
2023-05-22 23:06:30 +0200

    Back to development: 2.0.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
     read("README.md") + "\n\n" + read("CHANGES.rst") + "\n\n" + read("LICENSE")
 )
 
-version = "2.0.3"
+version = "2.0.4.dev0"
 
 setup(
     name="plone.api",
