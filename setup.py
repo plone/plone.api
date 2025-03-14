1debc4304b5069ea629c507f2e1818c1d95388f6
Maurits van Rees <maurits@vanrees.org>
2025-03-14 22:32:21 +0100

    Back to development: 2.4.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "2.4.0"
+version = "2.4.1.dev0"
 
 long_description = "\n".join(
     [Path("README.md").read_text(), Path("CHANGES.md").read_text()]
