bef714766f670afbac354554b2b6ee69b86bcaee
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-11-21 23:59:20 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "2.5.4.dev0"
+version = "3.0.0.dev0"
 
 long_description = "\n".join(
     [Path("README.md").read_text(), Path("CHANGES.md").read_text()]
