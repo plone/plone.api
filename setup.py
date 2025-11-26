001d75ab17077be93a50df163a4f79c2549033ca
Maurits van Rees <maurits@vanrees.org>
2025-11-26 21:43:31 +0100

    Bumped version for breaking release.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from setuptools import setup
 
 
-version = "3.0.0.dev0"
+version = "3.0.0a1.dev0"
 
 long_description = "\n".join(
     [Path("README.md").read_text(), Path("CHANGES.md").read_text()]
