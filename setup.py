a3ecfa738cd9829c95c7c0ae1c4df4ca2bff1c3d
Maurits van Rees <maurits@vanrees.org>
2026-03-25 09:21:16 +0100

    Back to development: 3.0.0a4
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from setuptools import setup
 
-version = "3.0.0a3"
+version = "3.0.0a4.dev0"
 
 long_description = "\n".join(
     [Path("README.md").read_text(), Path("CHANGES.md").read_text()]
